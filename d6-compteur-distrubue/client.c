aa455d170742e96248ceaabc48f2914a311ddb64
Ali Mar NGOM <122835666+alimar440@users.noreply.github.com>
2024-12-24 19:49:20 +0000

    devoir á rendre

---
+++ d6-compteur-distrubue/client.c
@@ -0,0 +1,98 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h> /* superset of previous */
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <unistd.h> 
+
+//pour tester : nc 0.0.0.0 port
+
+
+int main(int argc, char *argv[]) {
+
+
+	if (argc < 2) {
+		printf("Erreur : aucun fichier spécifié\n");
+		printf("Usage : %s  < PORT>\n", argv[0]);
+		return 1; 
+	}
+	
+
+    int socketClient  ;
+
+    if( (socketClient = socket(AF_INET, SOCK_STREAM, 0) )== -1 ) {
+        	perror("ERROR! creation de socket") ;
+            exit(1) ;
+    }
+
+    struct sockaddr_in sockaddr ;
+
+    memset( &sockaddr , 0 , sizeof(sockaddr)) ;
+
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_port = htons(atoi(argv[2]));
+    sockaddr.sin_addr.s_addr = inet_addr(argv[1]);
+    
+    
+    
+    if(  connect(socketClient , (struct sockaddr * ) &sockaddr , sizeof(sockaddr) )  == -1 ) {
+        	 perror("Erreur d'ouverture de connexion ") ;
+            exit(1) ;
+    }
+
+    printf("Execution execution connexion success 🥳 \n");
+
+
+
+    int nbSend , nbRcv ; 
+
+    int buffRcv;
+    int buffSnd;
+    int i = 0 ;
+    
+
+    while(1){
+
+	//memset(&buffRcv , 0 ,10) ;
+	nbRcv = recv(socketClient, &buffRcv, sizeof(buffRcv), 0);
+
+        printf("[Client# %d]  Recu : %d \n" ,i, buffRcv); 
+
+        sleep(1);
+	
+	buffRcv++ ;
+	
+	if (nbRcv == -1) {
+            perror("Erreur lors de recv");
+            break;
+        } else if (nbRcv == 0) {
+            printf("Le Server a fermé la connexion.\n");
+            break;
+        }
+
+		//recevoir ping
+		
+        nbSend = send( socketClient, &buffRcv , sizeof(buffRcv), 0) ;
+        
+      //  printf("[Client# %d]  Send : %s \n" ,i, buffSnd); 
+
+
+        i++ ;
+
+
+    }
+
+
+   close(socketClient) ;
+    return 0;
+}
+
+
+
+    
---
+++ d6-compteur-distrubue/server.c
@@ -0,0 +1,103 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h> /* superset of previous */
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <unistd.h> 
+
+//pour tester : nc 0.0.0.0 port
+
+
+int main(int argc, char *argv[]) {
+
+
+	if (argc < 2) {
+		printf("Erreur : aucun fichier spécifié\n");
+		printf("Usage : %s  < PORT>\n", argv[0]);
+		return 1; 
+	}
+	
+
+    int socketTCP ;
+
+    if( (socketTCP = socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
+        	perror("ERROR! creation de socket") ;
+            exit(1) ;
+    }
+
+    struct sockaddr_in sockaddr ;
+
+    memset( &sockaddr , 0 , sizeof(sockaddr)) ;
+
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_port = htons(atoi(argv[1]));
+    sockaddr.sin_addr.s_addr = inet_addr("0.0.0.0") ; 
+
+   
+
+    
+    if(bind(socketTCP , (struct sockaddr * ) &sockaddr , sizeof(struct sockaddr_in) )  == -1 ) {
+        	perror("ERROR! de  bind ") ;
+            exit(1) ;
+    }
+
+    printf("Execution execution bind success 🥳 \n");
+
+    if( listen(socketTCP , 5)  == -1 ) {
+        	perror("ERROR! de listen ") ;
+            exit(1) ;
+    }
+
+    printf("Execution execution listen success 🥳 \n");
+
+    struct sockaddr_in clientaddr ;
+    int tailleClt = sizeof(clientaddr) ;
+
+    int socketClientDistant = accept(socketTCP , (struct sockaddr * ) &clientaddr , &tailleClt) ;
+
+    int nbSend , nbRcv ; 
+
+    int buffRcv;
+    int buffSnd = 0 ;
+
+    int i = 0 ;
+    
+
+    while(1){
+
+        nbSend = send( socketClientDistant, &buffSnd , sizeof(buffSnd) , 0) ;
+    	
+    	
+        sleep(1);
+       
+	//recevoir ping
+	
+	
+	nbRcv = recv(socketClientDistant, &buffSnd ,sizeof(buffSnd), 0);
+	
+	
+	printf("[Server# %d] Recu : %d\n" ,i, buffSnd); 	
+	
+	buffSnd++ ;
+		
+	if (nbRcv == -1) {
+            perror("Erreur lors de recv");
+            break;
+        } else if (nbRcv == 0) {
+            printf("Le client a fermé la connexion.\n");
+            break;
+        }
+
+
+
+    }
+
+	close(socketClientDistant)  ;    
+     	return 0 ;
+}
